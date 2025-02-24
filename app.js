8bbd0d7810c425d06dc4c33133f0021ec2d76a24
Afamefuna Anekeh <afamefuna.anekeh@optimizely.com>
2025-02-24 11:21:33 -0600

    updated the port number for heroku dynamic port

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(express.static('public'));
 
 // ... rest of your code ...
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
